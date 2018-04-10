ec253f374224720208d23a2444d5db4869ea7842
Hannah Aurand <aurand.hannah@gmail.com>
2018-04-10 14:50:59 -0400

    Fix the AddProject component to render the form.

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ class App extends Component {
           </Link>
         </nav>
         <Switch>
-          <Route path="/project/:id" component={ProjectDetails} />
           <Route path="/project/create" component={AddProject} />
+          <Route path="/project/:id" component={ProjectDetails} />
           <Route path="/playground" component={Playground} />
           <Route path="/experience" component={Experience} />
           <Route path="/project" component={Projects} />
