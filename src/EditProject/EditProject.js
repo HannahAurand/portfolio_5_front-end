97c17ac08d4e11882eaaaca7fc0d8ee0264a75f7
Hannah Aurand <aurand.hannah@gmail.com>
2018-04-11 18:15:53 -0400

    Add handle edit function in edit project page

--- src/EditProject/EditProject.js
+++ src/EditProject/EditProject.js
@@ -13,17 +13,29 @@ class EditProject extends Component {
       link: '',
       image: ''
     }
+    this.handleEdit = this.handleEdit.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+}
+
+handleEdit() {
+    //need to set the state to the user's new inputs
+    this.setState({ [event.target.name]: event.target.value})
 }
         render() {
             return (
                 <div className="edit_project">
                 <form onSubmit={this.handleSubmit}>
-                Name<input name='name' type='text' value={this.state.name} onChange={this.handleEdit} placeholder={this.state.name}></input>
-                Description<input name='description' type='text' value={} onChange={this.handleEdit} placeholder={this.state.name}></input>
-                Languages<input name='languages' type='text' value={} onChange={this.handleEdit} placeholder={this.state.name}></input>
-                Link<input name='link' type='text' value={} onChange={this.handleEdit} placeholder={this.state.name}></input>
-                Image<input name='link' type='text' value={} onChange={this.handleEdit} placeholder={this.state.name}></input>
-                <button onClick='Submit'></button>
+                Name
+                <input name='name' type='text' value={this.state.name} onChange={this.handleEdit} placeholder={this.state.name} />
+                Description
+                <input name='description' type='text' value={} onChange={this.handleEdit} placeholder={this.state.name}/>
+                Languages
+                <input name='languages' type='text' value={} onChange={this.handleEdit} placeholder={this.state.name}/>
+                Link
+                <input name='link' type='text' value={} onChange={this.handleEdit} placeholder={this.state.name}/>
+                Image
+                <input name='link' type='text' value={} onChange={this.handleEdit} placeholder={this.state.name}/>
+                <button onClick='Submit'>Submit Edit</button>
                 </form>
                 </div>
             )
